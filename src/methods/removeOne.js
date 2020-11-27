8fed0fbcecfb1a14a2fecadfe93b55bc1268e734
denyncrawford <kiquerangelb@gmail.com>
2020-11-27 14:47:00 -0300

    removeOne fix

--- src/methods/removeOne.js
+++ src/methods/removeOne.js
@@ -5,15 +5,16 @@ export default async (filename, query) => {
   let stream = new ReadFileStream(filename);
   const queryMaker = new mingo.Query(query, {});
   let removed = [];
-  let update = []
+  let update = [];
   return new Promise((resolve, reject) => {
     stream.on('document', async obj => {
-      if (queryMaker.test(obj)) removed.push(obj);
-      if (queryMaker.test(obj) && removed.length) update.push(obj) 
+      if (queryMaker.test(obj) && !removed.length) removed.push(obj);
+      update.push(obj) 
+      if (removed.length && obj._id == removed[0]._id) update.pop()
     })
     stream.on('end', async () => {
       await updateFile(filename, update)
-      return resolve(removed[0]);
+      return resolve(removed[0] || null);
     })
   })
 }
