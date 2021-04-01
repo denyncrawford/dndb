034b24bf2c9929bdc99ab9f9ae7be48d2e99804c
Tom Wilson <twilson63@gmail.com>
2021-04-01 11:46:50 +0000

    update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -11,38 +11,38 @@ import Executor from './executor.ts'
 */
 
 class Datastore extends EventEmitter {
-    public filename: string;
-    private bufSize?: number;
-    private executor: Executor = new Executor();
+  public filename: string;
+  private bufSize?: number;
+  private executor: Executor = new Executor();
 
-    /**
-     * Builds the datastore with the given options.
-     * @constructor
-     * @param {object} options - Options to build the datastore.
-     */
+  /**
+   * Builds the datastore with the given options.
+   * @constructor
+   * @param {object} options - Options to build the datastore.
+   */
 
-    constructor({
-        filename,
-        autoload,
-        timeStamp,
-        bufSize
-    }: DataStoreOptions) {
-        super();
-        this.filename = filename ? resolve(Deno.cwd(), filename) : resolve(Deno.cwd(), "./database.json");
-        this.bufSize = bufSize;
-        if (autoload) this.loadDatabase().then(() => {
-            this.emit('load')
-        })
-    };
+  constructor({
+    filename,
+    autoload,
+    timeStamp,
+    bufSize
+  }: DataStoreOptions) {
+    super();
+    this.filename = filename ? resolve(Deno.cwd(), filename) : resolve(Deno.cwd(), "./database.json");
+    this.bufSize = bufSize;
+    if (autoload) this.loadDatabase().then(() => {
+      this.emit('load')
+    })
+  };
 
-    /**
-    * Loads the database on first load and ensures that path exists.
-    * @method
-    */
+  /**
+  * Loads the database on first load and ensures that path exists.
+  * @method
+  */
 
-    async loadDatabase() {
-        return init(this.filename)
-    }
+  async loadDatabase() {
+    return init(this.filename)
+  }
 
     /**
     * Finds multiple matching documents.
@@ -53,92 +53,94 @@ class Datastore extends EventEmitter {
     * @return Promise
     */
 
-    async find(query: { any: any }, projection: any = {}, cb?: (x: any) => any) {
-        if (cb && typeof cb == 'function') return cb(await this.executor.add(_find, [this.filename, query, projection, this.bufSize]));
-        return this.executor.add(_find, [this.filename, query, projection, this.bufSize])
-    }
+    type FindResults = { [k: string]: unknown }[]
 
-    /**
-    * Find first matching document.
-    * @method findOne
-    * @param {object | string} query - Query object
-    * @param {object | string} projection - Projection object
-    * @callback Optional callback
-    * @return Promise
-    */
+async find(query: { [k: string]: unknown }, projection: { [k: string]: number } = {}, cb ?: (x: FindResults) => void) {
+  if (cb && typeof cb == 'function') return cb(await this.executor.add(_find, [this.filename, query, projection, this.bufSize]));
+  return this.executor.add(_find, [this.filename, query, projection, this.bufSize])
+}
 
-    async findOne(query: { any: any }, projection: any = {}, cb?: (x: any) => any) {
-        projection = projection || {};
-        if (cb && typeof cb == 'function') return cb(await this.executor.add(_findOne, [this.filename, query, projection, this.bufSize]));
-        return this.executor.add(_findOne, [this.filename, query, projection, this.bufSize])
-    }
+/**
+* Find first matching document.
+* @method findOne
+* @param {object | string} query - Query object
+* @param {object | string} projection - Projection object
+* @callback Optional callback
+* @return Promise
+*/
 
-    /**
-    * Insert a document.
-    * @method insert
-    * @param {object} data - Data Insertion
-    * @callback Optional callback
-    * @return Promise
-    */
+async findOne(query: { any: any }, projection: any = {}, cb ?: (x: any) => any) {
+  projection = projection || {};
+  if (cb && typeof cb == 'function') return cb(await this.executor.add(_findOne, [this.filename, query, projection, this.bufSize]));
+  return this.executor.add(_findOne, [this.filename, query, projection, this.bufSize])
+}
 
-    async insert(data: any, cb?: (x: any) => any) {
-        if (cb && typeof cb == 'function') return cb(await this.executor.add(_insert, [this.filename, data]))
-        return this.executor.add(_insert, [this.filename, data])
-    }
+/**
+* Insert a document.
+* @method insert
+* @param {object} data - Data Insertion
+* @callback Optional callback
+* @return Promise
+*/
 
-    /**
-    * Update multiple matching documents
-    * @method update
-    * @param {object | string} query - Query object
-    * @param {object} operatos - Aggregation operators
-    * @callback Optional callback
-    * @return Promise
-    */
+async insert(data: any, cb ?: (x: any) => any) {
+  if (cb && typeof cb == 'function') return cb(await this.executor.add(_insert, [this.filename, data]))
+  return this.executor.add(_insert, [this.filename, data])
+}
 
-    async update(query: { any: any }, operators: any, cb?: (x: any) => any) {
-        if (cb && typeof cb == "function") return cb(await this.executor.add(_update, [this.filename, query, operators, this.bufSize]));
-        return this.executor.add(_update, [this.filename, query, operators, this.bufSize])
-    }
+/**
+* Update multiple matching documents
+* @method update
+* @param {object | string} query - Query object
+* @param {object} operatos - Aggregation operators
+* @callback Optional callback
+* @return Promise
+*/
 
-    /**
-    * Update first matching document
-    * @method updateOne
-    * @param {object | string} query - Query object
-    * @param {object} operatos - Aggregation operators
-    * @callback Optional callback
-    * @return Promise
-    */
+async update(query: { any: any }, operators: any, cb ?: (x: any) => any) {
+  if (cb && typeof cb == "function") return cb(await this.executor.add(_update, [this.filename, query, operators, this.bufSize]));
+  return this.executor.add(_update, [this.filename, query, operators, this.bufSize])
+}
 
-    async updateOne(query: { any: any }, operators: any, cb?: (x: any) => any) {
-        if (cb && typeof cb == "function") return cb(await this.executor.add(_updateOne, [this.filename, query, operators, this.bufSize]));
-        return this.executor.add(_updateOne, [this.filename, query, operators, this.bufSize])
-    }
+/**
+* Update first matching document
+* @method updateOne
+* @param {object | string} query - Query object
+* @param {object} operatos - Aggregation operators
+* @callback Optional callback
+* @return Promise
+*/
 
-    /**
-    * Remove multiple matching documents
-    * @method removeOne
-    * @param {object | string} query - Query object
-    * @callback Optional callback
-    * @return Promise
-    */
+async updateOne(query: { any: any }, operators: any, cb ?: (x: any) => any) {
+  if (cb && typeof cb == "function") return cb(await this.executor.add(_updateOne, [this.filename, query, operators, this.bufSize]));
+  return this.executor.add(_updateOne, [this.filename, query, operators, this.bufSize])
+}
 
-    async remove(query: any, cb?: (x: any) => any) {
-        if (cb && typeof cb == "function") return cb(await this.executor.add(_remove, [this.filename, query, this.bufSize]));
-        return this.executor.add(_remove, [this.filename, query, this.bufSize])
-    }
+/**
+* Remove multiple matching documents
+* @method removeOne
+* @param {object | string} query - Query object
+* @callback Optional callback
+* @return Promise
+*/
 
-    /**
-    * Remove first matching document
-    * @method removeOne
-    * @param {object | string} query - Query object
-    * @callback Optional callback
-    * @return Promise
-    */
+async remove(query: any, cb ?: (x: any) => any) {
+  if (cb && typeof cb == "function") return cb(await this.executor.add(_remove, [this.filename, query, this.bufSize]));
+  return this.executor.add(_remove, [this.filename, query, this.bufSize])
+}
 
-    async removeOne(query: any, cb?: (x: any) => any) {
-        if (cb && typeof cb == "function") return cb(await this.executor.add(_removeOne, [this.filename, query, this.bufSize]));
-        return this.executor.add(_removeOne, [this.filename, query, this.bufSize])
-    }
+/**
+* Remove first matching document
+* @method removeOne
+* @param {object | string} query - Query object
+* @callback Optional callback
+* @return Promise
+*/
+
+async removeOne(query: any, cb ?: (x: any) => any) {
+  if (cb && typeof cb == "function") return cb(await this.executor.add(_removeOne, [this.filename, query, this.bufSize]));
+  return this.executor.add(_removeOne, [this.filename, query, this.bufSize])
+}
 
 }
 
