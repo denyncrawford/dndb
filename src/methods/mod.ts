09fc5f3e0b19ec70344a2f23ea22173dad21261e
AmteurPotion <1k0j2i4@gmail.com>
2022-10-11 14:51:07 +0900

    Cleanup export by import_map

--- src/methods/mod.ts
+++ src/methods/mod.ts
@@ -1,7 +1,7 @@
-export { default as _find } from "./find.ts";
-export { default as _insert } from "./insert.ts";
-export { default as _findOne } from "./findOne.ts";
-export { default as _update } from "./update.ts";
-export { default as _updateOne } from "./updateOne.ts";
-export { default as _remove } from "./remove.ts";
-export { default as _removeOne } from "./removeOne.ts";
+export { default as _find } from "dndb/methods/find.ts";
+export { default as _insert } from "dndb/methods/insert.ts";
+export { default as _findOne } from "dndb/methods/findOne.ts";
+export { default as _update } from "dndb/methods/update.ts";
+export { default as _updateOne } from "dndb/methods/updateOne.ts";
+export { default as _remove } from "dndb/methods/remove.ts";
+export { default as _removeOne } from "dndb/methods/removeOne.ts";
